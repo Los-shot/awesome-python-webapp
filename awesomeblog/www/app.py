908a0959cb6d4f3590e811bd94aa6853c70f405f
G01fTure <543741914@qq.com>
2019-06-03 18:31:43 +0800

    app init

---
+++ awesomeblog/www/app.py
@@ -0,0 +1,25 @@
+import logging; logging.basicConfig(level=logging.INFO)#带分号的代码
+
+import asyncio, os, json, time
+from datetime import datetime
+
+from aiohttp import web
+
+import sql
+
+def index(request):
+    return web.Response(body = b'<h1>Awesome</h1>',content_type = 'text/html')
+
+@asyncio.coroutine
+def init(loop):
+    app = web.Application()
+    app.router.add_route('GET','/',index)
+    runner = web.AppRunner(app)
+    yield from runner.setup()
+    site = web.TCPSite(runner,'127.0.0.1',9000)
+    yield from site.start()
+    logging.info('server start at http://127.0.0.1:9000...')
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
