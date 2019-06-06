6d5a2dc3f37efb9b0c638ccdb4b85ebfeeca7c37
duzesen <543751914@qq.com>
2019-06-06 04:48:32 +0100

    web framework

--- awesomeblog/www/app.py
+++ awesomeblog/www/app.py
@@ -6,30 +6,47 @@ from datetime import datetime
 from aiohttp import web
 
 import sql
+from webframe import add_routes
 
-def get(path):
+async def logger_factory(app,handler):
+    async def logger(request):
+        logging.info('Request: %s %s' % (request.method,request.path))
+        return await handler(request)
+    return logger
 
-    '''
-    Define decorator @get('/path')
-    '''
-
-    def decorator(func):
-        def wrapper(*args,**kw):
-            return func(*args,**kw)
-
-        wrapper.__method__ = 'GET'
-        wrapper.__route__ = path
-
-        return wrapper
-
-    return decorator
-
+async def response_factory(app,handler):
+    async def response(request):
+        r = await handler(request)
+        if isinstance(r,web.StreamResponse):
+            return r
+        if isinstance(r,bytes):
+            resp = web.Resource(body = r)
+            resp.content_type = 'application/octet-stream'
+            return resp
+        if isinstance(r,str):
+            resp = web.Response(body = r.encode('utf-8'))
+            resp.content_type = 'text/html:charset=utf-8'
+            return resp
+        if isinstance(r,dict):
+            pass
+        
 def index(request):
     return web.Response(body = b'<h1>Awesome</h1>',content_type = 'text/html')
 
+def init_jinja2(app,filter = None):
+    pass
+
+def datetime_filter():
+    pass
+
+def add_static():
+    pass
+
 async def init():
-    app = web.Application()
-    app.router.add_route('GET','/',index)
+    app = web.Application(middlewares = [logger_factory,response_factory])
+    init_jinja2(app,filter = dict(datetime = datetime_filter))
+    add_routes(app,'handlers')
+    add_static(app)
     runner = web.AppRunner(app)
     await runner.setup()
     site = web.TCPSite(runner,'127.0.0.1',9000)
